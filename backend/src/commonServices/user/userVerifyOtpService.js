95cd10c730715fb4c8a6987d886723d957c5d930
Mahmud Hasan Mubin <mahmudhasanmubin1996@gmail.com>
2025-05-26 23:07:57 +0600

    removed some unused codes

--- backend/src/commonServices/user/userVerifyOtpService.js
+++ backend/src/commonServices/user/userVerifyOtpService.js
@@ -1,34 +1,3 @@
-// const otpModel = require("../../models/users/otpModel"); // Import the OTP model
-
-// const userVerifyOTPService = async (req, dataModel) => {
-//     try{
-//         let email = req.params.email;
-//         let OTP = req.params.otpCode.toString(); // Convert OTP to string if it's a number
-//         let status = 'active';
-//         let statusUpdate = 'Used';
-
-//         let otpCount = await otpModel.findOne({ email: email, otp: OTP, status: status});
-//         console.log(otpCount);
-//         if (otpCount.length > 0 && otpCount[0].total > 0) {
-// //            console.log(otpCount);
-//             await otpModel.updateOne(
-//                 { email: email, otp: OTP },
-//                 { $set: { status: statusUpdate } }
-//             );
-//             return { status: 200, message: "OTP Verified Successfully" };
-//         }else{
-//             return { status: 404, message: "Invalid OTP" };
-//         }
-//     }catch(err){
-//         console.log(err);
-//         return {status: 500, message: "Internal Server Error", error: err.message};
-//     }
-// }
-
-
-// module.exports = userVerifyOTPService;
-
-
 
 const otpModel = require("../../models/users/otpModel");
 
